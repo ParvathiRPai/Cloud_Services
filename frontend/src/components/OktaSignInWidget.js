a3af12729d211615bfcfb00fe2f4b59547071221
Shreya Ghotankar <70411701+shreyaghotankar@users.noreply.github.com>
2021-02-23 18:35:00 -0800

    okta sigin page fix

--- frontend/src/components/OktaSignInWidget.js
+++ frontend/src/components/OktaSignInWidget.js
@@ -1,67 +1,31 @@
 import React, { Component } from 'react';
-import { withOktaAuth } from '@okta/okta-react';
-import { OktaAuth } from '@okta/okta-auth-js';
+import ReactDOM from 'react-dom';
+import OktaSignIn from '@okta/okta-signin-widget';
+import '@okta/okta-signin-widget/dist/css/okta-theme.css';
 
-class OktaSignInWidget extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      sessionToken: null,
-      email: '',
-      password: ''
-    };
-
-    this.oktaAuth = new OktaAuth({
-      issuer: props.baseUrl,
-      clientId: '0oa2i1po7LTnjRN6S5d6',
-      pkce: true
+export default class OktaSignInWidget extends Component {
+  componentDidMount() {
+    const el = ReactDOM.findDOMNode(this);
+    this.widget = new OktaSignIn({
+      baseUrl: this.props.baseUrl,
+      authParams: {
+        // If your app is configured to use the Implicit Flow
+        // instead of the Authorization Code with Proof of Code Key Exchange (PKCE)
+        // you will need to uncomment the below line
+         // pkce: false
+      },
+      features:{
+      	registration: true
+      }
     });
-
-    this.handleEmailChange = this.handleEmailChange.bind(this);
-    this.handlePasswordChange = this.handlePasswordChange.bind(this);
-    this.signIn = this.signIn.bind(this);
+    this.widget.renderEl({el}, this.props.onSuccess, this.props.onError);
   }
 
-  handleEmailChange(event) {
-    this.setState({email: event.target.value});
-  }
-
-  handlePasswordChange(event) {
-    this.setState({password: event.target.value});
-  }
-
-  async signIn(event) {
-    event.preventDefault();
-    const transaction = await this.oktaAuth.signIn({
-      username: this.state.email,
-      password: this.state.password
-    });
-
-    if (transaction.status === 'SUCCESS') {
-      this.props.oktaAuth.signInWithRedirect({sessionToken: transaction.sessionToken})
-    } else {
-      throw new Error('Could not sign in: ' + transaction.status);
-    }
+  componentWillUnmount() {
+    this.widget.remove();
   }
 
   render() {
-    return (
-      <form onSubmit={this.signIn} className="login-form">
-        <h2>Log In</h2>
-        <p>Please login to continue</p>
-        <label className="full-width-input">
-          Email
-          <input type="text" placeholder="Email" value={this.state.email} onChange={this.handleEmailChange} required />
-        </label>
-        <label className="full-width-input">
-          Password
-          <input type="password" placeholder="Password" value={this.state.password} onChange={this.handlePasswordChange} required autoComplete="off" />
-        </label>
-        <button className="button">Login</button>
-      </form>
-    );
+    return <div />;
   }
 };
-
-export default withOktaAuth(OktaSignInWidget);
