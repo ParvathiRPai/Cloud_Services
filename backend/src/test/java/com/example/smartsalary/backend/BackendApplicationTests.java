dec960f92618450f77996a4a2d63ba3186695c7b
Parvathi <pava@uw.edu>
2021-05-12 14:40:11 -0700

    test cases

--- backend/src/test/java/com/example/smartsalary/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/example/smartsalary/backend/BackendApplicationTests.java
@@ -1,13 +1,18 @@
 package com.example.smartsalary.backend;
 
+import com.example.smartsalary.backend.controller.EmployeeController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+@SpringBootTest
 class BackendApplicationTests {
-
-//	@Test
+	@Autowired
+	private EmployeeController controller;
+	@Test
 	void contextLoads() {
+		assertThat(controller).isNotNull();
 	}
-
 }
